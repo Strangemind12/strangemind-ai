91b60c1d7d310196f7702ce6eaa51424af4dc7f9
Strangemind12 <143359170+Strangemind12@users.noreply.github.com>
2025-06-10 02:28:44 +0100

    command_router.py

--- command_router.py
+++ command_router.py
@@ -1,8 +1,9 @@
-# handlers/command_router.py
-from commands.admin import handle_admin_command
-from commands.user  import handle_user_command
+from helpers.consent import has_consented
 
-def route_message(phone, message, is_group, group_id, is_admin):
+def route_message(phone, message, is_group=False, group_id=None, is_admin=False):
+    if is_group and not has_consented(phone):
+        return  # Ignore unsolicited group messages
+
     if is_admin:
         handle_admin_command(phone, message, is_group, group_id)
     else:
